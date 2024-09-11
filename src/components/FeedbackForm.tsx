3d08c2c27c64a91afaa7b9072e743a0680ec277d
Pedro <pedrofilipetapadas@gmail.com>
2024-09-11 17:29:11 +0100

    added the form to write the feedback messages

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,3 +1,46 @@
+import { useState } from "react";
+import { MAX_LENGTH } from "../lib/constants";
+
 export default function FeedbackForm() {
-  return <div>FeedbackForm</div>;
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (message.length === 0) {
+      return alert("Please enter a message");
+    }
+    if (!message.includes("#")) {
+      return alert("Please include a #hashtag in your message");
+    }
+
+    const splitMessage = message.split("#").slice(1);
+
+    const companies = splitMessage.map((sliced) => sliced.split(" ")[0]);
+
+    alert(`Message: ${message}\nCompanies: ${companies.join(", ")}`);
+
+    setMessage("");
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <textarea
+        id="feedback-textarea"
+        placeholder="textarea"
+        spellCheck={false}
+        maxLength={MAX_LENGTH}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <label htmlFor="feedback-textarea">
+        Add your feedback here with the company #hashtag
+      </label>
+      <div>
+        <p className="u-italic">{MAX_LENGTH - message.length}</p>
+        <button type="submit">
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
+  );
 }
--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,3 +1,26 @@
+import { TriangleUpIcon } from "@radix-ui/react-icons";
+
 export default function FeedbackList() {
-  return <ol className="feedback-list">FeedbackList</ol>;
+  return (
+    <ol className="feedback-list">
+      <li className="feedback">
+        <button>
+          <TriangleUpIcon />
+          <span>123</span>
+        </button>
+        <div>
+          <p>T</p>
+        </div>
+        <div>
+          <p>Company</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam omnis
+            voluptatibus accusamus dolores nemo. Perferendis!
+          </p>
+        </div>
+
+        <p>1d</p>
+      </li>
+    </ol>
+  );
 }
